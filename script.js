fa5cf66152f75d6ce3f436467f751f6241ea4ea7
Ifazo <ziaulkarimifaz@gmail.com>
2022-08-23 16:34:49 +0600

    function of select-v done

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
+const players = [];
+
+function addToSelact(element) {
+  const playerName = element.parentNode.children[0].innerText;
+
+  const playerobj = {
+    playerName: playerName,
+  };
+
+  players.push(playerobj);
+
+  display(players);
+}
+
+function display(playerList) {
+
+
+  const tableBody = document.getElementById("player-list");
+
+    tableBody.innerHTML = "";
+
+  for (let i = 0; i < playerList.length; i++) {
+  
+    const name = players[i].playerName;
+
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+    <td>${i + 1}. ${name}</td>
+    `;
+
+    tableBody.appendChild(tr);
+  }
+}
+
 const element = document.getElementById("calculate");
 element.addEventListener("click", calculation);
 
 function calculation() {
   const perPlayerCost = document.getElementById("per-player").value;
   const perPlayer = parseInt(perPlayerCost);
-  const cost = perPlayer * 5;
+
+  const select = players.length;
+
+  const cost = perPlayer * select;
   document.getElementById("player-expenses").innerText = cost;
 }
 
