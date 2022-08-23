c70dfed17116ea7feaf04a10846f9fdfd754c98d
Ifazo <ziaulkarimifaz@gmail.com>
2022-08-23 22:11:31 +0600

    complete project

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const players = [];
 
+document.getElementById("myBtn1").addEventListener("click", disable1);
+function disable1() {
+  document.getElementById("myBtn1").disabled = true;
+}
+document.getElementById("myBtn2").addEventListener("click", disable2);
+function disable2() {
+  document.getElementById("myBtn2").disabled = true;
+}
+document.getElementById("myBtn3").addEventListener("click", disable3);
+function disable3() {
+  document.getElementById("myBtn3").disabled = true;
+}
+document.getElementById("myBtn4").addEventListener("click", disable4);
+function disable4() {
+  document.getElementById("myBtn4").disabled = true;
+}
+document.getElementById("myBtn5").addEventListener("click", disable5);
+function disable5() {
+  document.getElementById("myBtn5").disabled = true;
+}
+document.getElementById("myBtn6").addEventListener("click", disable6);
+function disable6() {
+  document.getElementById("myBtn6").disabled = true;
+}
+
 function addToSelact(element) {
   const playerName = element.parentNode.children[0].innerText;
 
@@ -12,17 +37,21 @@ function addToSelact(element) {
   display(players);
 }
 
-function display(playerList) {
-
-
+function display() {
   const tableBody = document.getElementById("player-list");
 
-    tableBody.innerHTML = "";
+  tableBody.innerHTML = "";
 
-  for (let i = 0; i < playerList.length; i++) {
-  
+
+
+  for (let i = 0; i < players.length; i++) {
+
+    if (players.length > 5) {
+      alert("You already select 5 players. You can't select more...");
+      players.pop();
+    }
+
     const name = players[i].playerName;
-
     const tr = document.createElement("tr");
 
     tr.innerHTML = `
@@ -31,6 +60,7 @@ function display(playerList) {
 
     tableBody.appendChild(tr);
   }
+
 }
 
 const element = document.getElementById("calculate");
@@ -48,6 +78,7 @@ function calculation() {
 
 const totalElement = document.getElementById("calculate-total");
 totalElement.addEventListener("click", totalCalculation);
+
 function totalCalculation() {
   const perPlayerCost = document.getElementById("player-expenses").innerText;
   const perPlayer = parseInt(perPlayerCost);
@@ -60,3 +91,4 @@ function totalCalculation() {
   const totalCost = perPlayer + manager + choach;
   document.getElementById("total").innerText = totalCost;
 }
+
